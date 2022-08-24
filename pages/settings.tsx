9428fb0e71b3d7e6733a10769853a16376d11502
a.urbanite <a.urbanite@gmail.com>
2022-08-24 13:29:54 +0200

    settings page styling and bugfix

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -4,12 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from "../reduxState/store";
 import { auth } from "../firebase-config";
 import { updateUser } from "../reduxState/authenticationSlice";
+import styles from '../styles/pages/Settings.module.css'
 
 const Settings = () => {
   const dispatch = useDispatch<AppDispatch>()
   const currentUser = useSelector((state: any) => state.currentUser)
-  const [displayname, setdisplayname] = useState<string>(currentUser.name);
-  const [email, setemail] = useState<string>(currentUser.email);
+  const [displayname, setdisplayname] = useState<string>(currentUser?.name);
+  const [email, setemail] = useState<string>(currentUser?.email);
   // const [photoURL, setphotoURL] = useState<string>(auth.currentUser!.photoURL!);
 
   const updateUserProfile = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -23,20 +24,29 @@ const Settings = () => {
   }
   
   return (
-    <>
+    <div className={styles.SettingsWrapper}>
       <h1>Settings</h1>
       <p>Welcome {displayname}! Change your profile information here.</p>
+      <br></br>
       {/* <img src={auth.currentUser!.photoURL!} className="profilePic" alt='profilePic'></img> */}
-      <form className='settingsForm' onSubmit={updateUserProfile}>
+      <form className={styles.settingsForm} onSubmit={updateUserProfile}>
         <label htmlFor="displayname">User name:</label>
-        <input id='displayname' onChange={(event) => {setdisplayname(event.target.value)}} defaultValue={displayname ? displayname : "currently no name assigned"}></input>
+        <input 
+          id='displayname' 
+          className={styles.settingsForm__input}
+          onChange={(event) => {setdisplayname(event.target.value)}} 
+          defaultValue={displayname ? displayname : "currently no name assigned"}/>
         {/* <label htmlFor="photoURL">photo URL:</label>
         <input id='photoURL' onChange={(event) => {setphotoURL(event.target.value)}} defaultValue={photoURL}></input> */}
         <label htmlFor="email">Email:</label>
-        <input id='email' onChange={(event) => {setemail(event.target.value)}} defaultValue={currentUser.email}/>
-        <input type="submit" value="Go!"/>
+        <input 
+          id='email' 
+          className={styles.settingsForm__input}
+          onChange={(event) => {setemail(event.target.value)}} 
+          defaultValue={currentUser?.email}/>
+        <input type="submit" value="Go!" className={styles.settingsForm__submit}/>
       </form>
-    </>
+    </div>
   )
 }
 
