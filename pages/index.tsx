d7e19650276117d08004b76c4c095a38c78fb95e
a.urbanite <a.urbanite@gmail.com>
2022-08-29 22:08:08 +0200

    changed initial fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import LeafletMapLoader from '../components/LeafletMap/LeafletMapLoader'
 import { setLocations } from '../reduxState/locationsSlice'
+import { env } from 'process';
 
 function HomePage(props: any) {
 
@@ -17,7 +18,9 @@ function HomePage(props: any) {
 }
 
 export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/api/locations')
+  const url = `${env.NEXT_ENV_HOST}/api/locations`
+  // console.log("ENV URL", url)
+  const res = await fetch(url)
   const locations = await res.json()
   return { props: { locations } }
 }
