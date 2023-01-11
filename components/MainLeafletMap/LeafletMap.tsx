2d0bd73a0235f7b4b376fddc44575728d8f1e68d
a.urbanite <a.urbanite@gmail.com>
2023-01-11 23:06:53 +0100

    cleanup

--- components/MainLeafletMap/LeafletMap.tsx
+++ components/MainLeafletMap/LeafletMap.tsx
@@ -1,50 +1,43 @@
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
-import 'leaflet-defaulticon-compatibility';
-import 'leaflet-draw/dist/leaflet.draw.css'
-import { FeatureGroup, MapContainer, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet'
-import styles from '../../pages/home/Home.module.css';
-
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-draw/dist/leaflet.draw.css";
+import { FeatureGroup, MapContainer, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
+import styles from "../../pages/home/Home.module.css";
 
 const LeafletMap = ({ locations }: any) => {
+  console.log(locations);
 
   return (
     <>
-      <MapContainer center={[52.5200, 13.4050]} zoom={13} scrollWheelZoom={true} className={styles.mapContainer}>
+      <MapContainer center={[52.52, 13.405]} zoom={13} scrollWheelZoom={true} className={styles.mapContainer}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {locations.map((location: any) => {
-          const timeObj =  Date.parse(location.properties.creationDate)
-          const date = new Date(timeObj).toLocaleDateString()
-         
+          const timeObj = Date.parse(location.properties.creationDate);
+          const date = new Date(timeObj).toLocaleDateString();
+
           return (
-          <GeoJSON data={location} key={location.properties.firebaseDocID}>
-            <Popup>
-              <b>{location.properties.featureName}</b> 
-              <br /> 
-              <b>description:</b> {location.properties.featureDescr}
-              <br /> 
-              {/* userID: {location.properties.firebaseUserID}
+            <GeoJSON data={location} key={location.properties.firebaseDocID}>
+              <Popup>
+                <b>{location.properties.featureName}</b>
+                <br />
+                <b>description:</b> {location.properties.featureDescr}
+                <br />
+                {/* userID: {location.properties.firebaseUserID}
               <br />  */}
-              <b>user name:</b> {location.properties.userName}
-              <br />
-              <b>creation date:</b> { date }
-            </Popup> 
-          </GeoJSON>)
-        }
-          
-          )}
-          
+                <b>user name:</b> {location.properties.userName}
+                <br />
+                <b>creation date:</b> {date}
+              </Popup>
+            </GeoJSON>
+          );
+        })}
       </MapContainer>
-      
     </>
-  )
+  );
+};
 
-}
-
-
-
-
-export default LeafletMap
+export default LeafletMap;
